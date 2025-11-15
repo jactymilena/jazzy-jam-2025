77120d73e94363de6e5fb579749f725b6485768b
Xavier Lemieux <43992877+dovakin7@users.noreply.github.com>
2025-11-15 08:19:13 -0500

    wall and floor spawning

---
+++ NinetyNinePinkBalls/Source/NinetyNinePinkBalls/MapGeneration/MapGenerator.cpp
@@ -0,0 +1,67 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MapGenerator.h"
+
+void AMapGenerator::BeginPlay()
+{
+	Super::BeginPlay();
+	GenerateMap();
+}
+
+TArray<FWallPosition> AMapGenerator::CalculateWallPositions()
+{
+	TArray<FWallPosition> WallPositions = {
+		FWallPosition{FVector(500.f, 500.f, 0.f), EWallOrientation::Vertical},
+		FWallPosition{FVector(1000.f, 1000.f, 0.f), EWallOrientation::Horizontal}
+	};
+	return WallPositions;
+}
+
+void AMapGenerator::GenerateMap()
+{
+	SpawnFloor();
+	SpawnWalls();
+}
+
+void AMapGenerator::SpawnMapElement(USceneComponent* ComponentToSpawn, const FVector& Position, const FRotator& Rotation)
+{
+	ComponentToSpawn->SetRelativeLocation(Position);
+	ComponentToSpawn->SetRelativeRotation(Rotation);
+			
+	ComponentToSpawn->RegisterComponent();
+	SpawnedMapElements.Add(ComponentToSpawn);
+}
+
+void AMapGenerator::SpawnFloor()
+{
+	for (int i =0; i< MapWidth; i++)
+	{
+		for (int j =0; j< MapHeight; j++)
+		{
+			UStaticMeshComponent* spawnedFloorTile = NewObject<UStaticMeshComponent>(this);
+			spawnedFloorTile->SetStaticMesh(FloorMeshes[0]);
+			const auto Position = FVector(TileSize * i, TileSize * j, 0.f);
+			
+			
+			SpawnMapElement(spawnedFloorTile, Position);
+		}
+	}
+}
+
+void AMapGenerator::SpawnWalls()
+{
+	TArray<FWallPosition> wallPositions = CalculateWallPositions();
+	for (FWallPosition wallPosition : wallPositions)
+	{
+		UStaticMeshComponent* wallToSpawn = NewObject<UStaticMeshComponent>(this);
+
+		wallToSpawn->SetStaticMesh(WallMeshes[0]);
+		
+		FRotator rotation = wallPosition.Orientation == EWallOrientation::Vertical 
+			? FRotator{} 
+			: FRotator(0, 90.f, 0.f);
+		
+		SpawnMapElement(wallToSpawn, wallPosition.Position, rotation);
+	}
+}
---
+++ NinetyNinePinkBalls/Source/NinetyNinePinkBalls/MapGeneration/MapGenerator.h
@@ -0,0 +1,61 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+
+// UE
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+
+// Generated
+#include "MapGenerator.generated.h"
+
+/**
+ * 
+ */
+enum class EWallOrientation
+{
+	Vertical = 0,
+	Horizontal,
+};
+
+struct FWallPosition
+{
+	FWallPosition(const FVector& Position, const EWallOrientation Orientation) : Position(Position), Orientation(Orientation) {}
+	FVector Position{};
+	EWallOrientation Orientation {};
+};
+
+UCLASS()
+class NINETYNINEPINKBALLS_API AMapGenerator : public AActor
+{
+	GENERATED_BODY()
+	
+	virtual void BeginPlay() override;
+	
+private:
+	TArray<FWallPosition> CalculateWallPositions();
+	void GenerateMap();
+	void SpawnMapElement(USceneComponent* ComponentToSpawn, const FVector& Position, const FRotator& Rotation = {});
+
+	void SpawnFloor();
+	void SpawnWalls();
+	
+	UPROPERTY(EditDefaultsOnly)
+	TArray<TWeakObjectPtr<USceneComponent>> SpawnedMapElements;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Map Elements")	
+	TArray<UStaticMesh*> WallMeshes;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Map Elements")	
+	TArray<UStaticMesh*> FloorMeshes;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Map Options")
+	int32 MapWidth;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Map Options")
+	int32 MapHeight;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Tile Settings")
+	float TileSize = 500.f;
+};
